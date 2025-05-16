5533703ee33b9abb41306662c5c492271c38403c
jarm32 <152169641+jarm32@users.noreply.github.com>
2025-05-16 16:57:41 +0200

    Delete LYRIC ANALYSIS/comprobar_azlyircs.py

--- LYRIC ANALYSIS/comprobar_azlyircs.py
+++
@@ -1,5 +0,0 @@
-with open("unpopus.txt", "r", encoding="latin-1") as file:
-    contenido = file.read()
-
-lista = contenido.split("---------------------------------------------------")
-print(len(lista)-1)
